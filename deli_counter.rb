0c185a53c0fc4fe420dfdaddeca574b8dfa3464e
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-14 23:03:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array.any?
     currenty_line = "The line is currently:"
     array.each_with_index do |name,index|
-      currenty_line << "#{index+1}. #{name}"
+      currenty_line << " #{index+1}. #{name}"
    end
     
    puts currenty_line
